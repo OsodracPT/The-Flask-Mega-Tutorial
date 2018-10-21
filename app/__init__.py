316e3273b83513ccd6885c447aa7c0283ca1e00f
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-10-22 00:46:14 +0100

    Part XIII: Languages

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,7 +11,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,6 +22,7 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
 
@@ -51,3 +53,8 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'pt'
